ade788eff74586f89e2827d2eef7aef561354d49
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:41:45 +0100

    Generato file types.js

--- dist/domain/types.js
+++ dist/domain/types.js
@@ -1,1 +1,17 @@
-"use strict";
+/**
+ * ENUM che definisce i tipi di mezzi di trasporto disponibili nel sistema
+ */
+export var TipoMezzo;
+(function (TipoMezzo) {
+    TipoMezzo["bici"] = "bici";
+    TipoMezzo["scooter"] = "scooter";
+    TipoMezzo["monopattino"] = "monopattino";
+})(TipoMezzo || (TipoMezzo = {}));
+/**
+ * ENUM che rappresenta lo stato corrente di un mezzo
+ */
+export var Stato;
+(function (Stato) {
+    Stato["disponibile"] = "disponibile";
+    Stato["InUso"] = "In uso";
+})(Stato || (Stato = {}));
