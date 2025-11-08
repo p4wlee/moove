2c6011b665bec0ad695c29063f66be85380c5e7c
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:33:03 +0100

    Commentato codice del file "citta.ts"

--- src/models/citta.ts
+++ src/models/citta.ts
@@ -1,12 +1,26 @@
 import {ICitta, IMezzo} from "../domain/types";
 
+/**
+ * CLASSE che implementa l'interfaccia ICitta e rappresenta una città
+ * Gestisce i mezzi disponibili nella città
+ */
 export class Citta implements ICitta {
+
+    // Array che contiene tutti i mezzi disponibili nella città
     public mezziDisponibili: IMezzo[] = []
     
+    /**
+    * COSTRUTTORE della classe Citta
+    */
     constructor (public nome: string) {}
 
+    /**
+     * METODO per aggiungere un mezzo alla flotta della città
+     */    
     aggiungiMezzo(mezzo:IMezzo): void {
+        // Aggiunge il mezzo all'array dei mezzi disponibili
         this.mezziDisponibili.push(mezzo);
+        // Console log di conferma dell' aggiunta del mezzo
         console.log(`${mezzo.tipo} id:#${mezzo.id} aggiunto a ${this.nome}`);
     }
 }
