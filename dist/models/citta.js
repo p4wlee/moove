bd9f40a442aba91556ec4d82fa6e06a9522c9104
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:41:57 +0100

    Generato file citta.js

--- dist/models/citta.js
+++ dist/models/citta.js
@@ -1,1 +1,23 @@
-"use strict";
+/**
+ * CLASSE che implementa l'interfaccia ICitta e rappresenta una città
+ * Gestisce i mezzi disponibili nella città
+ */
+export class Citta {
+    /**
+    * COSTRUTTORE della classe Citta
+    */
+    constructor(nome) {
+        this.nome = nome;
+        // Array che contiene tutti i mezzi disponibili nella città
+        this.mezziDisponibili = [];
+    }
+    /**
+     * METODO per aggiungere un mezzo alla flotta della città
+     */
+    aggiungiMezzo(mezzo) {
+        // Aggiunge il mezzo all'array dei mezzi disponibili
+        this.mezziDisponibili.push(mezzo);
+        // Console log di conferma dell' aggiunta del mezzo
+        console.log(`${mezzo.tipo} id:#${mezzo.id} aggiunto a ${this.nome}`);
+    }
+}
