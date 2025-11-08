999bc468d5650209b222354d88968e68cfee6ed6
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:42:07 +0100

    Generato file mezzo.js

--- dist/models/mezzo.js
+++ dist/models/mezzo.js
@@ -1,1 +1,28 @@
-"use strict";
+import { Stato } from "../domain/types";
+/**
+ * CLASSE che implementa l'interfaccia IMezzo
+ */
+export class Mezzo {
+    /**
+     * COSTRUTTORE con parametri pubblici
+     */
+    constructor(tipo, stato = Stato.disponibile) {
+        this.tipo = tipo;
+        this.stato = stato;
+        // Assegnazione ID univoco
+        this.id = Mezzo.prossimoId++;
+    }
+    /**
+    * METODO per assegnare un utente al mezzo
+    */
+    assegnaUtente(utente) {
+        if (this.stato === Stato.disponibile) {
+            this.stato = Stato.InUso;
+            console.log(`${utente.nome} ${utente.cognome} ha preso in uso il seguente mezzo: ${this.tipo} id:#${this.id}`);
+        }
+        else {
+            console.log(`Il ${this.tipo} id:#${this.id} non è momentaneamente disponibile`);
+        }
+    }
+}
+Mezzo.prossimoId = 1;
