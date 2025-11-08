3efdafb7c875f9feceb70a2eee8c13030aac218b
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:42:21 +0100

    Generato file utente.js

--- dist/models/utente.js
+++ dist/models/utente.js
@@ -1,1 +1,26 @@
-"use strict";
+import { Stato } from "../domain/types";
+/**
+ * CLASSE che implementa l'interfaccia IUtente
+ */
+export class Utente {
+    /**
+     * COSTRUTTORE con parametri pubblici
+     */
+    constructor(nome, cognome, email, metodoPagamento) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.metodoPagamento = metodoPagamento;
+    }
+    /**
+     * METODO per prenotare un mezzo
+     * */
+    prenotaMezzo(mezzo) {
+        if (mezzo.stato === Stato.disponibile) {
+            mezzo.assegnaUtente(this);
+        }
+        else {
+            console.log(`Mi dispiace ${this.nome} ${this.cognome}, il mezzo "${mezzo.tipo}" non è attualmente disponibile.`);
+        }
+    }
+}
