ccdc32a31db785d77a4ecaf7373e41823207fb87
Davide Paulicelli <paulicelli.davide@gmail.com>
2025-11-08 18:25:54 +0100

    Commentato codice del file "mezzo.ts"

--- src/models/mezzo.ts
+++ src/models/mezzo.ts
@@ -1,16 +1,26 @@
 import {IMezzo, IUtente, Stato, TipoMezzo} from "../domain/types";
 
+/**
+ * CLASSE che implementa l'interfaccia IMezzo
+ */
 export class Mezzo implements IMezzo {
     private static prossimoId = 1;
     public id: number;
 
+    /**
+     * COSTRUTTORE con parametri pubblici
+     */
     constructor (
 
         public tipo: TipoMezzo, 
         public stato: Stato = Stato.disponibile) {
+            // Assegnazione ID univoco
             this.id = Mezzo.prossimoId++; 
         }
 
+        /**
+        * METODO per assegnare un utente al mezzo
+        */        
         assegnaUtente (utente:IUtente) : void {
             if (this.stato === Stato.disponibile){
                 this.stato = Stato.InUso;
